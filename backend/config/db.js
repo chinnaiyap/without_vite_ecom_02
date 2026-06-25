e65e67268ffaee9c2d75896dbcdbd49a22e5d3ff
demo <chinnaiyaperiyasamy@gmail.com>
2026-06-25 10:28:29 +0530

    Fix API URL

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    // await mongoose.connect(process.env.DATABASE_URL);
     await mongoose.connect(process.env.MONGO_URI);
 
     console.log("MongoDB Connected");
